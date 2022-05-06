2b8a46e6ea277b0277d52a5da994f185d3226f93
Ishu Mehta <>
2022-05-06 23:29:49 +0530

    dropdown refined

--- src/COMPONENTS/Dropdown.js
+++ src/COMPONENTS/Dropdown.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { act } from 'react-dom/test-utils';
 
 const Dropdown = ({ options, selected, onSetSelected }) => {
+    const [visible, setVisible] = useState(null);
+    const [active, setActive] = useState(null);
 
     const displayMethod = options.map((option) => {
-        if (option != selected) {
+        if (option !== selected) {
             return (
                 <div key={option.value} className="item" onClick={() => { onSetSelected(option) }}>
                     {option.label}
@@ -12,14 +15,27 @@ const Dropdown = ({ options, selected, onSetSelected }) => {
         }
     })
 
+    const onListClick = () => {
+
+        if (visible == "visible") {
+            console.log("hey")
+            setVisible(null)
+            setActive(null)
+        } else {
+            console.log("hey yo")
+            setVisible("visible")
+            setActive("active")
+        }
+    }
+
     return (
         <div className='ui form'>
             <div className="field">
                 <div className="label">Select a color</div>
-                <div className="ui selection dropdown visible active">
+                <div className={"ui selection dropdown visible " + active} onClick={onListClick} >
                     <i className="dropdown icon"></i>
                     <div className="text">{selected.value}</div>
-                    <div className="menu visible transition">
+                    <div className={"menu " + visible + " transition"}>
                         {displayMethod}
                     </div>
                 </div>
